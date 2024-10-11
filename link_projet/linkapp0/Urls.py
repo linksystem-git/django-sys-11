3c1a9ff740d589d25dc37e87ddb045a2037c0a46
ambroise-ed <amroisenmz@gmail.com>
2024-10-11 16:10:58 +0100

    Mise à jour des fichiers de l'application

--- link_projet/linkapp0/Urls.py
+++ link_projet/linkapp0/Urls.py
@@ -1,9 +1,7 @@
-# urls.py
-
 from . import views
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .views import * 
 
 def is_superuser(user):
@@ -11,33 +9,36 @@ def is_superuser(user):
 
 urlpatterns = [
     path('admin/', user_passes_test(is_superuser)(admin.site.urls)),
-    path('', login_view, name='connexion'), 
-    path('home/', home, name='home'),  
-    path('Data/', datas, name='datas'),  
-    path('dossier/', page_dossiers, name='page_dossiers'),
-    path('prospects/', page_prospects, name='page_prospects'),
-    path('suivie/', suivie, name='suivie'),
-    path('profile/', profiles, name='profiles'),  
-    path('planning/', planning, name='planning'),   
-    path('clients/', page_clients, name='page_clients'), 
-    path('update_prospect/', views.edit_prospect, name='edit_prospect'),
-    path('delete_prospect/<int:prospect_id>/', views.delete_prospect, name='delete_prospect'),
+    path('', login_view, name='connexion'),  # Page de connexion, sans login_required
+
+    # Ajout de login_required à toutes les autres vues
+    path('home/', login_required(home), name='home'),  
+    path('Data/', login_required(datas), name='datas'),  
+    path('dossier/', login_required(page_dossiers), name='page_dossiers'),
+    path('prospects/', login_required(page_prospects), name='page_prospects'),
+    path('suivie/', login_required(suivie), name='suivie'),
+    path('profile/', login_required(profiles), name='profiles'),  
+    path('planning/', login_required(planning), name='planning'),   
+    path('clients/', login_required(page_clients), name='page_clients'), 
+    path('update_prospect/', login_required(views.edit_prospect), name='edit_prospect'),
+    path('delete_prospect/<int:prospect_id>/', login_required(views.delete_prospect), name='delete_prospect'),
 
     #----------------------gestion dossier------------------------------------------
-    path('supprimer-dossier/<int:dossier_id>/', supprimer_dossier, name='supprimer_dossier'),
-     path('modifier_dossier/', views.modifier_dossier, name='modifier_dossier'),
+    path('supprimer-dossier/<int:dossier_id>/', login_required(supprimer_dossier), name='supprimer_dossier'),
+    path('modifier_dossier/', login_required(views.modifier_dossier), name='modifier_dossier'),
+
     #----------------------gestion client--------------------------------------------------
-    path('supprimer_client/<int:client_id>/', views.supprimer_client, name='supprimer_client'),
+    path('supprimer_client/<int:client_id>/', login_required(views.supprimer_client), name='supprimer_client'),
+
     #----------------------gestion incident------------------------------------------
-    path('incidents/supprimer/<int:incident_id>/', supprimer_incident, name='supprimer_incident'),  # URL pour supprimer un incident
-    path('modifier_incident/<int:incident_id>/', views.modifier_incident, name='modifier_incident'),
+    path('incidents/supprimer/<int:incident_id>/', login_required(supprimer_incident), name='supprimer_incident'),
+    path('modifier_incident/<int:incident_id>/', login_required(views.modifier_incident), name='modifier_incident'),
+
     #----------------------gestion data------------------------------------------
-    path('data/create/', views.data_create, name='data_create'),
-    path('delete_data/', views.delete_data, name='delete_data'),
-    path('edit_data/', views.edit_data, name='edit_data'),
+    path('data/create/', login_required(views.data_create), name='data_create'),
+    path('delete_data/', login_required(views.delete_data), name='delete_data'),
+    path('edit_data/', login_required(views.edit_data), name='edit_data'),
 
     #---------------------deconnexion----------------------------------------
-    path('deconnexion/', logout_view, name='logout'),
-    
-
+    path('deconnexion/', login_required(logout_view), name='logout'),
 ]
